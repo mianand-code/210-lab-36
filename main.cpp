a14c3b85b25301ba04f0d6816fb2c3ad77868afd
Mira Anand <miraanand@comcast.net>
2024-11-27 22:29:52 -0800

    Initial Commit - Starting by cloning the starter repo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+// COMSC-210 | Lab 36 | Mira Anand
+// Module 14, Lesson: Binary Trees & Binary Search Trees, Assignment: Records BST
+// IDE used: Visual Studio Code for Mac
+
 #include <iostream>
 #include "IntBinaryTree.h"
 using namespace std;
 
-int main() {
+int main() 
+{
     
 
     return 0;
